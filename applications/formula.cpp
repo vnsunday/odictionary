ab51e8d82e9ee13573cefa5dd888a917512b9e54
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-17 12:24:24 +0700

    quality forming

---
+++ applications/formula.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void quality_form() 
+{
+    const char* azPosition1[][2] = {
+        { "Before", "Noun" },
+        { "After", "Verb" },
+        { "After", "Noun" }
+    };
+
+    const char* szQualityAware[][2] = {
+        { }
+    };
+
+    int n1 = sizeof(azPosition1)/sizeof(azPosition1[0]);
+}
+
+int main()
+{
+    return 0;
+}
